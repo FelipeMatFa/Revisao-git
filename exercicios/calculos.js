350d2664bd96adfe746192df844f10ae1125f790
FelipeMatFa <Felipe2006mattosdefarias@gmail.com>
2024-03-06 15:18:13 -0300

    exercicios

---
+++ exercicios/calculos.js
@@ -0,0 +1,38 @@
+// Exercicios de operações de calculos
+
+const num1 = 236;
+const num2 = 95;
+
+// Soma
+let soma = num1 + num2;
+
+console.log("Resultado: " + soma + "!");
+// console.log("Resultado:", soma);
+// console.log(`Resultado: ${soma} mil`);
+
+// subtração
+let subtracao = num1 - num2
+
+console.log("Subtração: " + subtracao + "!");
+
+// multiplicação
+let multiplicacao = num1 * num2
+
+console.log("Multiplicação: " + multiplicacao + "!");
+
+// divisão
+let divisao = num1/num2
+
+console.log("Divisão: " + divisao + "!");
+
+// numeros par ou impar
+if (num1 % 2 === 0){
+    console.log("Num1 é Par!")
+} else{
+    console.log("Num1 é Impar!")
+}
+if (num2 % 2 === 0){
+    console.log("Num2 é Par!")
+} else{
+    console.log("Num2 é Impar!")
+}
---
+++ exercicios/tabuada.js
@@ -0,0 +1,6 @@
+// Construir uma tabuada utilizando os codigos javascript
+let num1 = 3;
+
+for(let i = 1; i <= 10; i++){
+    console.log(`${num1} X ${i} = ${num1*i}`)
+}
